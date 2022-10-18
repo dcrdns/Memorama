1904bc70ad1ed0cff3a48026ab25c43fb43d3e73
daca129 <105086706+daca129@users.noreply.github.com>
2022-10-18 08:17:47 -0600

    Detecta cuando esta volteada la carta :)

--- js/script.js
+++ js/script.js
@@ -3,5 +3,14 @@ var cards = document.querySelectorAll('.card');
 [...cards].forEach((card)=>{
   card.addEventListener( 'click', function() {
     card.classList.toggle('is-flipped');
+    if(card.getAttribute('facing') === 'true') {
+      card.setAttribute('facing', 'false');
+      console.log(card.getAttribute('facing'))
+    }
+    else {
+      card.setAttribute('facing', 'true');
+      console.log(card.getAttribute('facing'))
+    }
   });
+
 });
