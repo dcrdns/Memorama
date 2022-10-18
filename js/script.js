f5b64548082414d621d10805004fc8884155df3d
daca129 <105086706+daca129@users.noreply.github.com>
2022-10-18 09:18:18 -0600

    Avance en que ya no se voltee

--- js/script.js
+++ js/script.js
@@ -3,11 +3,10 @@ let cont =0;
 let fiCart;
 
 [...cards].forEach((card)=>{
-  card.addEventListener( 'click', function() {
-    card.classList.toggle('is-flipped');
-    if(card.getAttribute('facing') === 'true') {
-      card.setAttribute('facing', 'false');
-      console.log(card.getAttribute('facing'))
+    if(card.getAttribute('facing') === 'false') {
+      card.addEventListener( 'click', function() {
+        card.classList.toggle('is-flipped');
+      });
     }
     else {
       card.setAttribute('facing', 'true');
@@ -15,24 +14,28 @@ let fiCart;
     }
   });
 
-});
+
 
 function acumu(car){
   cont =cont+1;
   if(cont==2){
     if(car.getAttribute('ident')==fiCart.getAttribute('ident')){
       console.log('son iguales chavo');
+      car.setAttribute('facing', 'true');
+      fiCart.setAttribute('facing', 'true');
     }
     else{
-      car.classList.toggle('is-flipped');
-      fiCart.classList.toggle('is-flipped');
+      setTimeout(() => {
+        car.classList.toggle('is-flipped');
+        fiCart.classList.toggle('is-flipped');
+      }, 1500);
+
     }
     cont=0;
-    fiCart=null;
   }
   else{
     fiCart=car;
 
   }
-
+console.log(cont);
 }
