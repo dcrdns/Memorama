2f1e646d31e264633617138fb3b929590503caea
daca129 <105086706+daca129@users.noreply.github.com>
2022-10-20 11:37:55 -0600

    Ya funciona, falta lo bonito.

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ var cards = document.querySelectorAll('.card');
 let cont =0;
 let fiCart;
 let b=0;
+let punt=0;
 
 // [...cards].forEach((card)=>{
 //       card.addEventListener( 'click', function() {
@@ -12,23 +13,33 @@ let b=0;
 //   });
 
 
-
 function acumu(car){
-  cont =cont+1;
   console.log(cont);
-  if(car.getAttribute('facing')!='true' & b==0){
+  if(car.getAttribute('facing')!='true' & b!=1){
     car.classList.toggle('is-flipped');
+    alt(car);
   }
+}
 
+function alt(car){
+  cont =cont+1;
   if(cont==2){//se seleccionaron dos cartas.
     if(car.getAttribute('id')!=fiCart.getAttribute('id')){//Entra a este if si las cartas seleccionada no es la misma carta.
       if(car.getAttribute('ident')==fiCart.getAttribute('ident')){//entra al if si las cartas si son pareja.
+        b=1;
         setTimeout(()=>{
           car.setAttribute('facing', 'true');
           fiCart.setAttribute('facing', 'true');
+          b=0;
         },100);
+        punt=punt+2;
+        document.getElementById('lpun').innerHTML='Puntos: '+punt
+        if (punt==12){
+          window.alert('ganaste');
+        }
         cont=0;
       }else if(car.getAttribute('facing')=='false' | fiCart.getAttribute('facing')=='false'){//entra a este else cuando seleccionaste las dos cartas pero no son pareja.
+        b=1;
         setTimeout(() => {
           car.classList.toggle('is-flipped');
           fiCart.classList.toggle('is-flipped');
